d2cfb671c22671d2c35a92d13af1fe8a703728b3
kgnot <hricaurtem@proton.me>
2025-05-18 12:20:04 -0500

    ErrorResponse: Cree la clase estatica que es el error response | Henry Ricaurte Mora

--- src/main/java/com/sid/portal_web/error/GlobalExceptionHandler.java
+++ src/main/java/com/sid/portal_web/error/GlobalExceptionHandler.java
@@ -5,4 +5,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+
+
+
+
+    @lombok.Data
+    @lombok.Builder
+    static class ErrorResponse {
+        private String message;
+        private int status;
+    }
 }
